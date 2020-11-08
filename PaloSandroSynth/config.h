dd042ed51bfcb05eb7c8437fd501acb9d8c59054
francescocretti <francesco.cretti@gmail.com>
2020-11-08 19:27:47 +0000

    update changelog for v0.6.1

--- PaloSandroSynth/config.h
+++ PaloSandroSynth/config.h
@@ -1,7 +1,7 @@
 #define PLUG_NAME "PaloSandroSynth"
 #define PLUG_MFR "FrancescoCretti"
-#define PLUG_VERSION_HEX 0x00000600
-#define PLUG_VERSION_STR "0.6.0"
+#define PLUG_VERSION_HEX 0x00000601
+#define PLUG_VERSION_STR "0.6.1"
 #define PLUG_UNIQUE_ID 'SoXA'
 #define PLUG_MFR_ID 'Acme'
 #define PLUG_URL_STR "https://iplug2.github.io"
